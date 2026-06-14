8c37010ad24f0fbfa35e20ced58b395ed2d20eab
Matheus Henrique Vieira Borges <matheushenriquevieiraborges@gmail.com>
2026-06-14 18:09:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// ─────────────────────────────────────────────────────────────
-//  Configuração do Supabase
-//  Este ficheiro NÃO deve ser enviado para o repositório público.
-//  Copie config.example.js para config.js e preencha os valores.
-// ─────────────────────────────────────────────────────────────
-const SUPABASE_URL = 'https://ckpqjgpilpijtlhwgyop.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrcHFqZ3BpbHBpanRsaHdneW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTkwOTksImV4cCI6MjA5MzM5NTA5OX0.NmToY7jNlbtt0L1gvPhnQtpnXblqkD5zup4coDD4UEU';
